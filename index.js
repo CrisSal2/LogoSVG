2d603fa3ce907cb75196cea7cd4ddd3b63dd2646
CrisSal2 <salgado.chris.m@gmail.com>
2024-06-24 11:23:03 -0700

    Added prompt layout

---
+++ index.js
@@ -0,0 +1,53 @@
+/******************************************************** Imports *************************************************************/
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+/***************************************************** Inquirer Prompt ********************************************************/
+
+
+function promptUser() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message:
+            "Enter up to three characters for your logo text:",
+          name: "text",
+        },
+        {
+          type: "input",
+          message:
+            "Enter color or a hexadecimal number for your text color:",
+          name: "textColor",
+        },
+        {
+          type: "list",
+          message: "What shape would you like your logo to render?",
+          choices: ["Triangle", "Square", "Circle"],
+          name: "shape",
+        },
+        {
+          type: "input",
+          message:
+            "Enter color or a hexadecimal number for your shape color",
+          name: "shapeBackgroundColor",
+        },
+      ])
+      .then((answers) => {
+        if (answers.text.length > 3) {
+          console.log("Must enter a value of no more than 3 characters");
+          promptUser();
+        } else {
+          writeToFile("logo.svg", answers);
+        }
+      });
+};
+
+
+/******************************************** Initializes function ********************************************/
+
+
+promptUser();
+  
