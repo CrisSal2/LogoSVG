135d55672bd8d5a218ce7630f490907e175dcc8e
CrisSal2 <salgado.chris.m@gmail.com>
2024-06-26 00:04:36 -0700

    reformatted code

--- index.js
+++ index.js
@@ -2,8 +2,40 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const writeToFile = require("./lib/generateSVG");
+/* const writeToFile = require("./lib/generateSVG"); */
 const { Triangle, Square, Circle } = require("./lib/shapes");
+
+
+/********************************************* Function to render using answers ************************************************/
+
+function writeToFile(fileName, answers) {
+
+  let svgString = "";
+  svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  svgString += "<g>";
+  svgString += `${answers.shape}`;
+
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+  }
+
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  svgString += "</g>";
+  svgString += "</svg>";
+
+  fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
+  });
+};
 
 
 /***************************************************** Inquirer Prompt ********************************************************/
@@ -33,7 +65,7 @@ function createLogo() {
         {
           type: "input",
           message:
-            "Enter color or a hexadecimal number for your shape color",
+            "Enter color or a hexadecimal number for your shape color:",
           name: "shapeBackgroundColor",
         },
       ])
@@ -48,7 +80,7 @@ function createLogo() {
 };
 
 
-/**************************************************** Initializes function **************************************************/
+/****************************************************** Initializes function ****************************************************/
 
 
 createLogo();
