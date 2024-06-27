a3374702a2a4fcd3e3f17d9fc6e6d6f977d4e325
CrisSal2 <salgado.chris.m@gmail.com>
2024-06-26 21:29:31 -0700

    reformatted code

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 /******************************************************** Imports *************************************************************/
 
-const inquirer = require("inquirer");
-const fs = require("fs");
-/* const writeToFile = require("./lib/generateSVG"); */
-const { Triangle, Square, Circle } = require("./lib/shapes");
+const inquirer = require('inquirer');
+const fs = require('fs');
+/* const writeToFile = require('./lib/generateSVG'); */
+const { Triangle, Square, Circle } = require('./lib/shapes');
 
 
 /********************************************* Function to render using answers ************************************************/
 
 function writeToFile(fileName, answers) {
 
-  let svgString = "";
+  let svgString = '';
   svgString =
     '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-  svgString += "<g>";
+  svgString += '<g>';
   svgString += `${answers.shape}`;
 
   let shapeChoice;
-  if (answers.shape === "Triangle") {
+  if (answers.shape === 'Triangle') {
     shapeChoice = new Triangle();
-    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
-  } else if (answers.shape === "Square") {
+    svgString += `<polygon points='150, 18 244, 182 56, 182' fill='${answers.shapeBackgroundColor}'/>`;
+  } else if (answers.shape === 'Square') {
     shapeChoice = new Square();
-    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+    svgString += `<rect x='73' y='40' width='160' height='160' fill='${answers.shapeBackgroundColor}'/>`;
   } else {
     shapeChoice = new Circle();
-    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+    svgString += `<circle cx='150' cy='115' r='80' fill='${answers.shapeBackgroundColor}'/>`;
   }
 
-  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-  svgString += "</g>";
-  svgString += "</svg>";
+  svgString += `<text x='150' y='130' text-anchor='middle' font-size='40' fill='${answers.textColor}'>${answers.text}</text>`;
+  svgString += '</g>';
+  svgString += '</svg>';
 
   fs.writeFile(fileName, svgString, (err) => {
-    err ? console.log(err) : console.log("Generated logo.svg");
+    err ? console.log(err) : console.log('Generated logo.svg');
   });
 };
 
@@ -45,36 +45,36 @@ function createLogo() {
     inquirer
       .prompt([
         {
-          type: "input",
+          type: 'input',
           message:
-            "Enter up to three characters for your logo text:",
-          name: "text",
+            'Enter up to three characters for your logo text:',
+          name: 'text',
         },
         {
-          type: "input",
+          type: 'input',
           message:
-            "Enter color or a hexadecimal number for your text color:",
-          name: "textColor",
+            'Enter color keyword or a hexadecimal number for your text color:',
+          name: 'textColor',
         },
         {
-          type: "list",
-          message: "What shape would you like your logo to render?",
-          choices: ["Triangle", "Square", "Circle"],
-          name: "shape",
+          type: 'list',
+          message: 'What shape would you like your logo to render?',
+          choices: ['Triangle', 'Square', 'Circle'],
+          name: 'shape',
         },
         {
-          type: "input",
+          type: 'input',
           message:
-            "Enter color or a hexadecimal number for your shape color:",
-          name: "shapeBackgroundColor",
+            'Enter color keyword or a hexadecimal number for your shape color:',
+          name: 'shapeBackgroundColor',
         },
       ])
       .then((answers) => {
         if (answers.text.length > 3) {
-          console.log("Must enter a value of no more than 3 characters");
+          console.log('Must enter a value of no more than 3 characters');
           createLogo();
         } else {
-          writeToFile("logo.svg", answers);
+          writeToFile('logo.svg', answers);
         }
       });
 };
